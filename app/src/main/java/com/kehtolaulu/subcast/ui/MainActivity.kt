c48beae98b2727e516f122b2506057f65a54828b
kehtolaulu <emilia270627@gmail.com>
2019-03-16 21:48:59 +0300

    add bottom navigation

--- app/src/main/java/com/kehtolaulu/subcast/ui/MainActivity.kt
+++ app/src/main/java/com/kehtolaulu/subcast/ui/MainActivity.kt
@@ -2,12 +2,34 @@ package com.kehtolaulu.subcast.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import com.kehtolaulu.subcast.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+//    private val mOnNavigationItemSelectedListener =
+//        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.navigation_downloads -> {
+//                setFragment(homeFragment)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_recycler -> {
+//                setFragment(recyclerFragment)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_pager -> {
+//                setFragment(pagerFragment)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
 }
