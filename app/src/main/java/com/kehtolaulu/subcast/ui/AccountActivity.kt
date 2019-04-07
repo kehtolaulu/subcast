e4786c70acb6f49b1c3b51979c1f2f4aea1ead40
kehtolaulu <emilia270627@gmail.com>
2019-04-07 18:19:54 +0300

    add account activity

---
+++ app/src/main/java/com/kehtolaulu/subcast/ui/AccountActivity.kt
@@ -0,0 +1,13 @@
+package com.kehtolaulu.subcast.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kehtolaulu.subcast.R
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
