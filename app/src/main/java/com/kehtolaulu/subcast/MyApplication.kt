ce66a896423ce76a3760d332f52ae23c5aa6e8fe
kehtolaulu <emilia270627@gmail.com>
2019-04-27 19:46:03 +0300

    fix technical issues

--- app/src/main/java/com/kehtolaulu/subcast/MyApplication.kt
+++ app/src/main/java/com/kehtolaulu/subcast/MyApplication.kt
@@ -3,17 +3,22 @@ package com.kehtolaulu.subcast
 import android.app.Application
 import com.kehtolaulu.subcast.di.components.AppComponent
 import com.kehtolaulu.subcast.di.components.DaggerAppComponent
+import com.kehtolaulu.subcast.di.modules.ApiModule
 import com.kehtolaulu.subcast.di.modules.AppModule
+import com.kehtolaulu.subcast.di.modules.NetModule
+import com.kehtolaulu.subcast.di.modules.PlayerModule
 
 class MyApplication : Application() {
     companion object {
-        var appComponent: AppComponent? = null
+        lateinit var appComponent: AppComponent
     }
 
     override fun onCreate() {
         super.onCreate()
         appComponent = DaggerAppComponent.builder()
             .appModule(AppModule(this))
+            .netModule(NetModule())
+            .apiModule(ApiModule())
             .build()
     }
 }
