a13fb10018548618b5c8ed7fa1a7ddb19e301f17
kehtolaulu <emilia270627@gmail.com>
2019-05-06 19:49:05 +0300

    delete navcomp and add classic bottom navigation

--- app/src/main/java/com/kehtolaulu/subcast/ui/MainActivity.kt
+++ app/src/main/java/com/kehtolaulu/subcast/ui/MainActivity.kt
@@ -5,35 +5,79 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
-import androidx.navigation.Navigation
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.facebook.stetho.Stetho
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kehtolaulu.subcast.MyApplication
 import com.kehtolaulu.subcast.R
 import com.kehtolaulu.subcast.adapters.EpisodesAdapter
 import com.kehtolaulu.subcast.adapters.PodcastsAdapter
+import com.kehtolaulu.subcast.di.components.DaggerMainComponent
+import com.kehtolaulu.subcast.di.components.DaggerPlayerComponent
+import com.kehtolaulu.subcast.di.modules.MainModule
+import com.kehtolaulu.subcast.di.modules.PlayerModule
 import com.kehtolaulu.subcast.entities.Episode
 import com.kehtolaulu.subcast.entities.Podcast
+import com.kehtolaulu.subcast.services.TokenService
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(),
     EpisodesAdapter.EpisodeOnClickListener, PodcastsAdapter.PodcastOnClickListener {
-    override fun onClick(podcast: Podcast) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
+    @Inject
+    lateinit var tokenService: TokenService
     var listener: OnQueryTextListener? = null
+
+    override fun onClick(podcast: Podcast) {
+        setDetailsFragment(podcast)
+    }
 
     override fun onClick(episode: Episode) {
         val intent = Intent(this, PlayerActivity::class.java)
-        intent.putExtra("id", episode.id)
+        intent.putExtra("episode", episode)
         startActivity(intent)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        initDagger()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        navigation.setupWithNavController(navController)
+        navigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_downloads -> {
+                    val fragment = DownloadsFragment()
+                    setFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_queue -> {
+                    val fragment = QueueFragment()
+                    setFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_favourites -> {
+                    val fragment = FavouritesFragment()
+                    setFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_later -> {
+                    val fragment = LaterFragment()
+                    setFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_account -> {
+                    if (tokenService.getToken() == null) {
+                        val fragment = LoginFragment()
+                        setFragment(fragment)
+                    } else {
+                        val fragment = SyncFragment()
+                        setFragment(fragment)
+                    }
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
         Stetho.initializeWithDefaults(this)
     }
 
@@ -43,8 +87,9 @@ class MainActivity : AppCompatActivity(),
         val searchViewItem = menu?.findItem(R.id.action_search)
         val searchView: SearchView = searchViewItem?.actionView as SearchView
         searchView.queryHint = "Search podcasts"
-        searchView.setOnSearchClickListener { doSearchFragmentTransaction() }
-        //TODO переписать под RX
+        searchView.setOnSearchClickListener {
+            setSearchFragment()
+        }
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 if (query.isEmpty()) return true
@@ -58,17 +103,47 @@ class MainActivity : AppCompatActivity(),
                 return true
             }
         })
-
         return super.onCreateOptionsMenu(menu)
     }
 
-    private fun doSearchFragmentTransaction() {
+    private fun setSearchFragment() {
         val fragmentManager = supportFragmentManager
         val fragment = SearchFragment()
         listener = fragment
         fragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment, fragment)
+            .replace(R.id.container, fragment)
             .commit()
+    }
+
+    private fun setDetailsFragment(podcast: Podcast) {
+        val fragmentManager = supportFragmentManager
+        val fragment = DetailsFragment.newInstance(podcast)
+        fragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    fun setSyncFragment() {
+        setFragment(SyncFragment())
+    }
+
+    fun setLoginFragment() {
+        setFragment(LoginFragment())
+    }
+
+    private fun initDagger() {
+        DaggerMainComponent.builder()
+            .appComponent(MyApplication.appComponent)
+            .mainModule(MainModule())
+            .build()
+            .inject(this)
     }
 
     interface OnQueryTextListener {
