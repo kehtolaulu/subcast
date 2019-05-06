8a28bafdc930e30585a13b94b058e05d40fa0fdb
kehtolaulu <emilia270627@gmail.com>
2019-05-06 19:50:56 +0300

    add presenters

---
+++ app/src/main/java/com/kehtolaulu/subcast/presenters/LaterPresenter.kt
@@ -0,0 +1,8 @@
+package com.kehtolaulu.subcast.presenters
+
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpPresenter
+import com.kehtolaulu.subcast.views.LaterView
+
+@InjectViewState
+class LaterPresenter() : MvpPresenter<LaterView>()
