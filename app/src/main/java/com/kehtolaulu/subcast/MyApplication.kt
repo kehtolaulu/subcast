27242be4c89a9d4ba6c740b399b7ebac42232f78
kehtolaulu <emilia270627@gmail.com>
2019-05-16 21:08:32 +0300

    inject app

--- app/src/main/java/com/kehtolaulu/subcast/MyApplication.kt
+++ app/src/main/java/com/kehtolaulu/subcast/MyApplication.kt
@@ -3,10 +3,6 @@ package com.kehtolaulu.subcast
 import android.app.Application
 import com.kehtolaulu.subcast.di.components.AppComponent
 import com.kehtolaulu.subcast.di.components.DaggerAppComponent
-import com.kehtolaulu.subcast.di.modules.ApiModule
-import com.kehtolaulu.subcast.di.modules.AppModule
-import com.kehtolaulu.subcast.di.modules.NetModule
-import com.kehtolaulu.subcast.di.modules.PlayerModule
 
 class MyApplication : Application() {
     companion object {
@@ -16,9 +12,8 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         appComponent = DaggerAppComponent.builder()
-            .appModule(AppModule(this))
-            .netModule(NetModule())
-            .apiModule(ApiModule())
+            .application(this)
+            .context(this)
             .build()
     }
 }
