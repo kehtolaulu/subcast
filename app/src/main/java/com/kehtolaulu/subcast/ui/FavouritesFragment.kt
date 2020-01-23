ae8075b53f9fa1a94a8f530e86852a20e58729f7
kehtolaulu <emilia270627@gmail.com>
2020-01-23 18:58:07 +0300

    firebase deploy

--- app/src/main/java/com/kehtolaulu/subcast/ui/FavouritesFragment.kt
+++ app/src/main/java/com/kehtolaulu/subcast/ui/FavouritesFragment.kt
@@ -7,9 +7,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.kehtolaulu.subcast.R
-import java.lang.AssertionError
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
